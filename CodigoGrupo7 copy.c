0f452edcbade68c3d8ae6284de4310e01883c38a
Nitoreto <lei.lioret@gmail.com>
2022-11-29 18:15:30 +0100

    holas

--- CodigoGrupo7 copy.c
+++ CodigoGrupo7 copy.c
@@ -24,56 +24,6 @@
 #define Der       3 //El 22 en el GPIO
 
 
-
-#define	TRUE	            (1==1)
-#define	FALSE	            (!TRUE)
-#define CHAN_CONFIG_SINGLE  8
-#define CHAN_CONFIG_DIFF    0
-
-static int myFd;
-
-char *usage = "Usage: mcp3008 all|analogChannel[1-8] [-l] [-ce1] [-d]";
-// -l   = load SPI driver,  default: do not load
-// -ce1  = spi analogChannel 1, default:  0
-// -d   = differential analogChannel input, default: single ended
-
-void loadSpiDriver() {
-	
-    if (system("gpio load spi") == -1) {
-		
-        fprintf(stderr, "Can't load the SPI driver: %s\n", strerror (errno));
-		
-        exit(EXIT_FAILURE);
-		
-    }
-	
-}
-
-void spiSetup(int spiChannel) {
-	
-    if ((myFd = wiringPiSPISetup(spiChannel, 1000000)) < 0) {
-		
-        fprintf(stderr, "Can't open the SPI bus: %s\n", strerror(errno));
-		
-        exit(EXIT_FAILURE);
-		
-    }
-	
-}
-
-int myAnalogRead(int spiChannel, int channelConfig, int analogChannel) {
-	
-    if(analogChannel < 0 || analogChannel > 7)
-        return -1;
-		
-    unsigned char buffer[3] = {1}; // start bit
-	
-    buffer[1] = (channelConfig + analogChannel) << 4;
-    wiringPiSPIDataRW(spiChannel, buffer, 3);
-	
-    return ((buffer[1] & 3) << 8) + buffer[2]; // get last 10 bits
-	
-}
 void Parar(){
     //15 el medio, 16 para arriba avanzar
     softPwmWrite(Izq, 15 );
